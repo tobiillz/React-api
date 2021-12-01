42a5ca0378301598c94b4e46262916865bc075e9
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-12-01 07:47:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        items: [],
+        isLoaded : false,
+      }
+    }
+  
+    componentDidMount(){
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+              this.setState({
+                isLoaded:true,
+                items:json,
+              })
+            })
+
+    }
+
+
+    render(){
+
+      var {isLoaded, items} = this.state;
+
+      if (!isLoaded){
+        return <div>Loading...</div>
+      }
+
+      else {
+          
+        return(
+          <div className="App">
+            <ul>
+              {items.map(item =>(
+                <li key={item.id}>
+                    Name: {item.name} | Email: {item.email}
+                </li>
+              ))}
+            </ul>
+         </div>
+
+        );
+       
+      }
+    }
+    
+
 }
 
 export default App;
+
+
+
+
+
